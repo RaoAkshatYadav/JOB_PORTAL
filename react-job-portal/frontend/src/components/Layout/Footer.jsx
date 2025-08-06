7f8578e4807dabc88d7e59269dd58eba5ba4a9c9
Akshat Yadav <yakshat01@gmail.com>
2025-08-06 10:04:50 +0530

    Your commit message here

--- react-job-portal/frontend/src/components/Layout/Footer.jsx
+++ react-job-portal/frontend/src/components/Layout/Footer.jsx
@@ -4,16 +4,17 @@ import {Link} from "react-router-dom"
 import { FaGithub , FaLinkedin} from "react-icons/fa"
 import { SiLeetcode } from "react-icons/si";
 import { RiInstagramFill} from "react-icons/ri"
+import { FaXTwitter } from 'react-icons/fa6';
 function Footer() {
   const {isAuthorized}  = useContext(Context)
   return (
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Vivek.</div>
+<div>&copy; All Rights Reserved by Akshat Yadav.</div>
 <div>
-  <Link to={'https://github.com/VIVEKKR79'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/vivek70489/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/vivek-kumar-ba306a260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/thevivek_25/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+  <Link to={'https://github.com/RaoAkshatYadav'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://x.com/iam_akshagtyadav'} target='X'><FaXTwitter></FaXTwitter></Link>
+  <Link to={'https://www.linkedin.com/in/akshat-yadav-3b1665233?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+  <Link to={'https://www.instagram.com/iam_akshatyadav/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
 </div>
       
     </footer>
